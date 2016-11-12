f0a90a6ec51d469dba4942d54ccbedcbd67e7211
Nithi <Nithi@nitharsansmbp2.fritz.box>
2016-11-12 20:01:43 +0100

    implemented parseDataFromCsvFile()

--- src/com/climatechangeapp/server/CsvDataImport.java
+++ src/com/climatechangeapp/server/CsvDataImport.java
@@ -2,18 +2,55 @@ package com.climatechangeapp.server;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 //import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import com.climatechangeapp.client.Temperature;
 
+
+
 //import com.google.api.server.spi.IoUtil;
 
 public class CsvDataImport {
 	
+	public ArrayList<ArrayList<String>> parseDataFromCsvFile()
+	{
+	     ArrayList<ArrayList<String>> dataFromFile = new ArrayList<ArrayList<String>>();
+	     try{
+	    	 // scans csv file and breaks into different token, between "," 
+	         Scanner scanner=new Scanner(new FileReader("/Users/Nithi/Desktop/Software Engineering"));
+	         //scanner.useDelimiter(",");
+	         
+	         //token converted in String
+	         //String line split between "," and saved in array list
+	         while(scanner.hasNextLine())
+	         {
+	            String dataInRow=scanner.nextLine();
+	            String []dataInRowArray=dataInRow.split(",");
+	            ArrayList<String> rowDataFromFile=new ArrayList<String>(Arrays.asList(dataInRowArray));
+	            dataFromFile.add(rowDataFromFile);
+	         }
+	         scanner.close();
+	     }catch (FileNotFoundException e){
+	        e.printStackTrace();
+	     }
+	     for(ArrayList<String> row : dataFromFile)
+	     {
+	    	 for(String s : row)
+	    	 {
+	    		 System.out.print(s + " | ");
+	    	 }
+	    	 System.out.println();
+	     }
+	     return dataFromFile;
+	}
+	
+	/*
 	public static List<Temperature> importCsv() throws IOException {
 		List<Temperature> temperatures = new ArrayList<Temperature>();
 		
@@ -42,5 +79,6 @@ public class CsvDataImport {
 		
 		return temperatures;
 	}
+	*/
 }
 
