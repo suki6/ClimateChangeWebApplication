e8263d0eaa9307ed7dab0729a73cfd4c3c84b9d3
suki6 <sukirthan.sundaralingam@gmail.com>
2016-11-28 14:56:20 +0100

    testdata

--- test/com/climatechangeapp/client/TimeLineRangeFilterTest.java
+++ test/com/climatechangeapp/client/TimeLineRangeFilterTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.google.gwt.core.client.JsArrayMixed;
+import com.google.gwt.core.client.JsonUtils;
 import com.googlecode.gwt.charts.client.controls.filter.NumberRangeFilterOptions;
 
 public class TimeLineRangeFilterTest {
@@ -13,9 +15,11 @@ public class TimeLineRangeFilterTest {
 		NumberRangeFilterOptions numberRangeFilterOptions = NumberRangeFilterOptions.create();
 		numberRangeFilterOptions.setMinValue(1850);
 		
+		JsArrayMixed dataArray = JsonUtils
+				.unsafeEval("[['City', 'Age'],['Zürich' , 1980],['Berlin', 2000],['Paris', 1970],['Toronto', 1954],['Sydney', 2002],['New York', 1903],['Rio', 1942],['Moscow', 1933]]");
+
 		
-		
-//		assertEquals(1850, );
+	//	assertEquals();
 	}
 
 }
--- test/com/climatechangeapp/client/WorldMapTest.java
+++ test/com/climatechangeapp/client/WorldMapTest.java
@@ -4,10 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import com.googlecode.gwt.charts.client.ColumnType;
+import com.googlecode.gwt.charts.client.DataTable;
+
 public class WorldMapTest {
 
 	@Test
 	public void test() {
+	/**	
+		DataTable dataTable1 = DataTable.create();
+		dataTable1.addColumn(ColumnType.STRING, "Country");
+		dataTable1.addColumn(ColumnType.STRING, "City");
+		dataTable1.addColumn(ColumnType.NUMBER, "Temperature");
+		dataTable1.addRows(7);
+		dataTable1.setValue(0, 0, "Germany");
+		dataTable1.setValue(0, 1, "Berlin");
+		dataTable1.setValue(0, 2, 12);
+		dataTable1.setValue(1, 0, "United States");
+		dataTable1.setValue(1, 1, "New York");
+		dataTable1.setValue(1, 2, 7);
+		dataTable1.setValue(2, 0, "Brazil");
+		dataTable1.setValue(2, 1, "Rio");
+		
+	//	assertEquals(3, dataTable1.getNumberOfRows());
+*/
+		
+		
 	}
 
 }
