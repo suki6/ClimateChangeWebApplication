3830d494ce48874996fd73f30f081220a9736031
suki6 <sukirthan.sundaralingam@gmail.com>
2016-12-08 16:19:24 +0100

    more tests

---
+++ test/com/climatechangeapp/client/ClimateChangeApplicationTest.java
@@ -0,0 +1,17 @@
+package com.climatechangeapp.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClimateChangeApplicationTest {
+
+	@Test
+	public void test() {
+
+		ClimateChangeApplication clima = new ClimateChangeApplication();
+		
+		assertNotNull(clima);
+	}
+
+}
